0c5b574b63f0a9d66b82cc2eced4f1859f3bc471
savhel <blife1255@gmail.com>
2025-11-13 16:16:37 +0100

    ajout des fichiers pour les mails apres commandes

---
+++ app/Mail/OrderConfirmationMail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderConfirmationMail extends Mailable
+{
+
+}
